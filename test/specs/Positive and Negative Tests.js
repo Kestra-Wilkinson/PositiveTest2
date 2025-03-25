d439274e40a49ed38ca12ab5708c345144c20314
Kestra-Wilkinson <kestra.wilkinson6530@stu.mtec.edu>
2025-03-24 23:42:34 -0600

    Update Positive and Negative Tests.js

--- test/specs/Positive and Negative Tests.js
+++ test/specs/Positive and Negative Tests.js
@@ -2,26 +2,17 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-  describe ('Positive Tests', () => {
-        it('should log in successfully with standard_user')
-        it('should log in successfully with locked_out_user')
-        it('should log in successfully with problem_user')
-        it('should log in successfully with performance_glitch_user')
-        it('should log in successfully with error_user')
-        it('should log in successfully with visual_user')
+ describe('Positive Tests', () => {
+  ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user', 'error_user', 'visual_user']
+    .forEach(user => it(`should log in successfully with ${user}`, () => {
+    
+    }));
+});
 
 
-})
-
-
-
-describe ('Negative Tests', () => {
-    it('should fail to log in  with Standard_user')
-    it('should fail to log in with Locked_out_user')
-    it('should fail to log in with  Problem_user')
-    it('should fail to log  in with Performance_glitch_user')
-    it('should fail to  log in with Error_user')
-    it('should fail to  log in  with Visual_user')
-
-
-})
+describe('Negative Tests', () => {
+  ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user', 'error_user', 'visual_user']
+    .forEach(user => it(`should fail to log in with ${user} using invalid credentials`, () => {
+    
+    }));
+});
