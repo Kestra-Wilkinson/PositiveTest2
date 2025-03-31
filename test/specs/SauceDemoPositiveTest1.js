0d82d9730fbd9aedfba1e0a83a0320d9e210590c
Kestra Wilkinson <kestra.wilkinson6530@stu.mtec.edu>
2025-03-31 12:25:52 -0600

    changes

--- test/specs/SauceDemoPositiveTest1.js
+++ test/specs/SauceDemoPositiveTest1.js
@@ -4,31 +4,45 @@ import SecurePage from '../pageobjects/secure.page.js'
 
 
 
-describe ("SauceDemo standard user Login Test", () => {
-    it("should log in with a valid user", async () => {
-       // await browser.url("https://www.saucedemo.com/");
-        
-        // const usernameInput = await $("#user-name");
-        // const passwordInput = await $("#password");
-        // const loginButton = await $("#login-button");
-        
-        // await usernameInput.setValue("standard_user");
-        // await passwordInput.setValue("secret_sauce");
-        // await loginButton.click();
-    describe("sauceDemo PositiveTests",() => {
-        it("should log in with a valid user", async () => {
-      await LoginPage.open()
-      await LoginPage.positiveTest('standard_user', 'secret_sauce') 
-      await LoginPage.positiveTest('locked_out_user','secret_sauce')
-      await LoginPage.positiveTest('problem_user','secret_sauce')
-      await LoginPage.positiveTest('performance_glitch_user','secret_sauce')
-      await LoginPage.positiveTest('error_user','secret_sauce')
-      await LoginPage.positiveTest('visual_user','secret_sauce')
-   
+
+describe("sauceDemo locked_out_user PositiveTest", () => {
+    it("should log in with locked_out_user", async () => {
+        await LoginPage.open();
+        await LoginPage.positiveTest('locked_out_user', 'secret_sauce');
     });
-   
-   
-      describe("SauceDemo NegativeTest", () => {
+});
+
+describe("sauceDemo problem_user PositiveTest", () => {
+    it("should log in with problem_user", async () => {
+        await LoginPage.open();
+        await LoginPage.positiveTest('problem_user', 'secret_sauce');
+        // await LoginPage.logout();
+    });
+});
+
+describe("sauceDemo performance_glitch_user PositiveTest", () => {
+    it("should log in with performance_glitch_user", async () => {
+        await LoginPage.open();
+        await LoginPage.positiveTest('performance_glitch_user', 'secret_sauce');
+        // await LoginPage.logout();
+    });
+});
+
+describe("sauceDemo error_user PositiveTest", () => {
+    it("should log in with error_user", async () => {
+        await LoginPage.open();
+        await LoginPage.positiveTest('error_user', 'secret_sauce');
+        // await LoginPage.logout();
+    });
+});
+
+
+    });
+});
+
+
+
+describe("SauceDemo NegativeTest", () => {
         it("should fail to log in with a invalid user", async () => {
       
       await LoginPage.open()
